8fb8142b863c8c88cea01aa447fd1341fc3c274c
Mike Hemberger <mike@thestizmedia.com>
2021-05-17 10:54:39 -0400

    update dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '64a9858806e8c7dba2cfe5a6f075026a4cfdf49b',
+    'name' => 'maithemewp/mai-installer',
+  ),
+  'versions' => 
+  array (
+    'afragen/wp-dependency-installer' => 
+    array (
+      'pretty_version' => '3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '65570a1eb966bb9adfc9d9e521357da080fbdc5a',
+    ),
+    'collizo4sky/persist-admin-notices-dismissal' => 
+    array (
+      'pretty_version' => '1.4.4',
+      'version' => '1.4.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '900739eb6b0ec0210465f5983a6d4e0e420289e4',
+    ),
+    'maithemewp/mai-installer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64a9858806e8c7dba2cfe5a6f075026a4cfdf49b',
+    ),
+  ),
+);
