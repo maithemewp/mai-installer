c75a60e8ddf24e09fd8922e71291418a5105f663
Mike Hemberger <mike@thestizmedia.com>
2021-11-29 14:44:10 -0500

    add back composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'db7c3adcc7930e6e85a8e805aaa459422f9b76d8',
+    'name' => 'maithemewp/mai-installer',
+  ),
+  'versions' => 
+  array (
+    'maithemewp/mai-installer' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db7c3adcc7930e6e85a8e805aaa459422f9b76d8',
+    ),
+  ),
+);
