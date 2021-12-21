bbd34ee4290fca0f0bdc4ce352a3e53850cba2be
Mike Hemberger <mike@thestizmedia.com>
2021-12-21 18:48:06 -0500

    wpdi changes and vbump 1.6.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '244cf7604a6b34a4662c29385497d4257f175956',
+    'name' => 'maithemewp/mai-installer',
+  ),
+  'versions' => 
+  array (
+    'afragen/wp-dependency-installer' => 
+    array (
+      'pretty_version' => '4.2.3',
+      'version' => '4.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '68aa4bfabfe0e807c7cf370580d0254b7b25c4f7',
+    ),
+    'afragen/wp-dismiss-notice' => 
+    array (
+      'pretty_version' => '0.2.3',
+      'version' => '0.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '21949732f1ea21d81500bdda8f882ac7eb0f98af',
+    ),
+    'maithemewp/mai-installer' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '244cf7604a6b34a4662c29385497d4257f175956',
+    ),
+  ),
+);
